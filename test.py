20fb1aa79bee85690c01bc9d4a263bf5642d83fc
mrgumpt <76255503+mrgumpt@users.noreply.github.com>
2024-06-16 11:40:04 +0100

    added successful first test on my VM

--- test.py
+++ test.py
@@ -2,15 +2,31 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
-def test():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
+class TestAmazon:
+    driver = ''
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.get('https://www.amazon.es/')
 
-    driver.get('https://www.amazon.es/')
-    search = driver.find_element(By.ID, 'twotabsearchtextbox')
-    search.send_keys('guitar', Keys.ENTER)
+    def test_guitar(self):
+        search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
+        search.send_keys('guitar', Keys.ENTER)
 
-    expected_text = '"guitar"'
-    actual_text = driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
+        expected_text = '"guitar"'
+        actual_text = self.driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
 
-    assert expected_text == actual_text, f'Error. Expected text "{expected_text}" but got "{actual_text}"'
+        assert expected_text == actual_text, f'Error. Expected text "{expected_text}" but got "{actual_text}"'
+        print('Success!')
+
+    def test_guitarPick(self):
+        search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
+        search.send_keys('guitar pick', Keys.ENTER)
+
+        expected_text = '"guitar pick"'
+        actual_text = self.driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
+
+        assert expected_text == actual_text, f'Error. Expected text "{expected_text}" but got "{actual_text}"'
+
+    def teardown_method(self):
+        self.driver.quit()
