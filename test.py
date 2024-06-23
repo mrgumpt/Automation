af1578838fdf1fb6cb197c5bfee0ef4cf9a6d614
mrgumpt <76255503+mrgumpt@users.noreply.github.com>
2024-06-23 19:56:03 +0100

    added parameters

--- test.py
+++ test.py
@@ -1,32 +1,27 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 class TestAmazon:
+    search_words = ('guitar','guitar pick','guitar straps')
+
     driver = ''
     def setup_method(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.driver.get('https://www.amazon.es/')
+    @pytest.mark.parametrize("search_query", search_words)
 
-    def test_guitar(self):
+    def test_guitar(self, search_query):
         search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
-        search.send_keys('guitar', Keys.ENTER)
+        search.send_keys(search_query, Keys.ENTER)
 
-        expected_text = '"guitar"'
+        expected_text = f'\"{search_query}\"'
         actual_text = self.driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
 
         assert expected_text == actual_text, f'Error. Expected text "{expected_text}" but got "{actual_text}"'
         print('Success!')
-
-    def test_guitarPick(self):
-        search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
-        search.send_keys('guitar pick', Keys.ENTER)
-
-        expected_text = '"guitar pick"'
-        actual_text = self.driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
-
-        assert expected_text == actual_text, f'Error. Expected text "{expected_text}" but got "{actual_text}"'
 
     def teardown_method(self):
         self.driver.quit()
