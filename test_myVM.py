b4d250eebdfa9125b7025cd8d435ec5cc1fc7f3b
mrgumpt <76255503+mrgumpt@users.noreply.github.com>
2024-06-23 20:54:52 +0100

    added test in GO to go to voice manager and check the Outbound Campaigns copy

--- test_myVM.py
+++ test_myVM.py
@@ -5,20 +5,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def testMyVM():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
-    wait = WebDriverWait(driver, 10)
+class TestMyVM:
+    driver = ''
 
-    driver.get('https://rferreira-dev.finesource.org/login.html')
-    email = driver.find_element(By.XPATH,"//input[@name='email']")
-    password = driver.find_element(By.XPATH,"//input[@name='password']")
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.get('https://rferreira-dev.finesource.org/login.html')
 
-    email.send_keys('teste@rferreira-dev.go-contact.com', Keys.ENTER)
-    password.send_keys('gocontact', Keys.ENTER)
+    def testMyVM(self):
 
-    expected_text = 'teste'
-    username_element = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@id='profile-userName']")))
-    actual_text = username_element.text
+        email = self.driver.find_element(By.XPATH,"//input[@name='email']")
+        password = self.driver.find_element(By.XPATH,"//input[@name='password']")
 
-    assert expected_text == actual_text, f'Error. {expected_text} is not equal to {actual_text}'
+        email.send_keys('teste@rferreira-dev.go-contact.com', Keys.ENTER)
+        password.send_keys('gocontact', Keys.ENTER)
+        wait = WebDriverWait(self.driver, 10)
+
+        expected_text = 'teste'
+        username_element = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@id='profile-userName']")))
+        actual_text = username_element.text
+
+        assert expected_text == actual_text, f'Error. {expected_text} is not equal to {actual_text}'
+
+        open_menu = wait.until(EC.visibility_of_element_located((By.XPATH, "//i[@class='fa fa-arrow-circle-left hit']")))
+        open_menu.click()
+
+        open_voiceManager = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@class='menu-item-parent' and @data-translate='mainMenu.voiceManager']")))
+        wait.until(EC.element_to_be_clickable(open_voiceManager)).click()
+
+        outboundCampaignsCopy = wait.until(EC.visibility_of_element_located((By.XPATH,"//h2[@id='outboundCampaigns']")))
+        actualCopy = outboundCampaignsCopy.text
+
+        expected_copy = 'Outbound Campaigns'
+        assert actualCopy == expected_copy, f'Error. {expected_copy} is not equal to {outboundCampaignsCopy}'
+
+    def teardown_method(self):
+        print("It's finished!")
