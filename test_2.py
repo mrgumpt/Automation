6268b91b5fd93bba9e3b3a77f2ad0042100c2897
mrgumpt <76255503+mrgumpt@users.noreply.github.com>
2024-06-30 10:29:09 +0100

    added test in GO to go to voice manager and check the Outbound Campaigns copy

---
+++ test_2.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TestAmazon:
+    driver = ''
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.get('https://www.amazon.es/')
+
+    def test_shop_cart(self):
+        self.driver.find_element(By.ID, 'nav-cart').click()
+        actual_text = self.driver.find_element(By.XPATH, '//*[@id="sc-empty-cart"]/div[2]/div[1]/h2').text
+        expected_text = 'Tu cesta de Amazon está vacía'
+
+        assert expected_text == actual_text, f'Expected text "{expected_text}" but got "{actual_text}"'
+
+        actual_items = self.driver.find_element(By.XPATH, '//*[@id="nav-cart-count"]').text
+        expected_items = '0'
+        assert expected_items == actual_items, f'Expected "{expected_items}" but got "{actual_items}"'
+
+
+    def teardown_method(self):
+        self.driver.quit()
