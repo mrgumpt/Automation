24b0d80e0407aa3167e0b83480a50028cd68b804
mrgumpt <76255503+mrgumpt@users.noreply.github.com>
2024-10-01 14:09:39 +0100

    GC audios

---
+++ Code Wars/create_phone_number.py
@@ -0,0 +1,3 @@
+def create_phone_number():
+    for i in range(0,10):
+        
--- Projects/CURA Healthcare Service/first_test.py
+++ Projects/CURA Healthcare Service/first_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import org.openqa.selenium.support.ui.Select
+
 
 
 class Test_CURA:
